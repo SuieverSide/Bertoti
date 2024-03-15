425234ebd30a4e42b1b9ccf0112e3d2ee81b6d1c
Lucas Lima <31674205+SuieverSide@users.noreply.github.com>
2024-03-15 20:26:02 -0300

    Delete lab3/meu primeiro REST directory

--- lab3/meu primeiro REST/def func (n,m).py
+++
@@ -1,6 +0,0 @@
-def func (n,m):
-    if (n>0) or (m>0):
-        return(n,m)
-    
-func(n-1, m-1)
-print(n, m) 
--- lab3/meu primeiro REST/teste.py
+++
@@ -1,7 +0,0 @@
-def teste(x):
-    x = 6
-    if x == 0 or x == 1:
-        return x
-    else:
-        return teste(x - 1)* x + teste(x - 2)
-total = 
