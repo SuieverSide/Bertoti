1c6c75031513635fc866033e2319991a22ac175d
Lucas Lima <lucas.limamp@outlook.com>
2024-03-15 21:37:10 -0300

    Changes

--- lab3/crud/src/main/java/com/crud/crud/RestController.java
+++ lab3/crud/src/main/java/com/crud/crud/RestController.java
@@ -2,10 +2,7 @@ package com.crud.crud;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +22,12 @@ public class RestController {
 
         listLocais.add(locais);
         return ResponseEntity.status(HttpStatus.OK).body("Adicionado com sucesso");
-    
     }
+    @DeleteMapping
+    ResponseEntity removeLocais(@RequestBody Locais locais){
+
+        
+        return ResponseEntity.status(HttpStatus.OK).body("Deletado com sucesso");
+    }
+
 }
